02303e2f3112bc7bddeab1035612d17a98dcab17
Jaclyn <jaclyndshahan@gmail.com>
2019-08-06 13:55:39 -0500

    fizzbuzz challenge

--- exercises/FizzBuzz/index.js
+++ exercises/FizzBuzz/index.js
@@ -17,6 +17,15 @@
  */
 
 function fizzBuzz(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log('fizzbuzz')
+    } else if (n % 5 === 0) {
+        console.log('buzz')
+    } else if (n % 3 === 0) {
+        console.log('fizz')
+    } else {
+        console.log(n)
+    }
 
 }
 
