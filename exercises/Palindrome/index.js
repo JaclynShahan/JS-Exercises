620f0ddea36c9d911005c0c0e996a5baf35c0442
Jaclyn <jaclyndshahan@gmail.com>
2019-08-08 19:53:23 -0500

    completed palindrome section

--- exercises/Palindrome/index.js
+++ exercises/Palindrome/index.js
@@ -14,7 +14,19 @@
  */
 
 function palindrome(str) {
-
-}
+    console.log(str)
+    let splitStr = str.split("")
+    console.log(splitStr)
+    let reverseArr = splitStr.reverse()
+    console.log(reverseArr)
+    let answer = reverseArr.join("")
+    console.log(answer)
+    if (str === answer) {
+      return true
+    } else {
+      return false
+    }
+    
+  }
 
 module.exports = palindrome;
