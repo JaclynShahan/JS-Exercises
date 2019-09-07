debbda89d8a6adfbc469314454b54d7989769198
Jaclyn <jaclyndshahan@gmail.com>
2019-09-06 20:42:30 -0500

    working on pyramid section

--- exercises/Pyramid/index.js
+++ exercises/Pyramid/index.js
@@ -17,7 +17,20 @@
  */
 
 function pyramid(n) {
-
+    for (i = 1; i <= n; i++) {
+       const arr = []
+    
+    for (j = 0; j < n - i; j++) {
+        arr.push('#');
+    }
+    for (j = 0; j < (2 * i) - 1; j++) {
+        arr.push('#')
+    }
+    for (j = 0; j < n - i; j++) {
+        arr.push('#')
+    }
+    console.log(arr.push('#'))
+}
 }
 
 module.exports = pyramid;
